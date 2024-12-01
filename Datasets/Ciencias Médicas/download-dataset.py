5edeea60f68525384d03fa888f29b47c89bfa80c
nao_lahera <lahera0103@gmail.com>
2024-12-01 12:26:28 -0500

    Marco teorico

--- Datasets/Ciencias Médicas/download-dataset.py
+++ Datasets/Ciencias Médicas/download-dataset.py
@@ -19,14 +19,53 @@ url = "https://revmedicina.sld.cu/index.php/med/issue/archive"
 driver.get(url)
 time.sleep(10) 
 
-time_slices = driver.find_element(By.ID, "issues").find_elements(By.CSS_SELECTOR, 'div')
+time_slices = driver.find_element(By.ID, "issues").find_elements(By.TAG_NAME, 'div')
+other_time_slices = []
+print('Founded issues')
+
+# link_template = 'https://revmedicina.sld.cu/index.php/med/issue/view/'
 time_slices_refs = []
 for item in time_slices:
-    element = item.find_element(By.CSS_SELECTOR, 'div').find_element(By.CLASS_NAME, 'a')
-    time_slices_refs.append((element.text, element.get_attribute('href')))
+    date = item.find_element(By.TAG_NAME, 'h3').text
+    print('Time slice: ', date)
+    
+    current_path = os.join(path, date)
+    os.makedirs(current_path)
+    print('Current_path: ', current_path)
+    
+    a_s = item.find_elements(By.XPATH, './/a')
+    for a in a_s:
+        ref = a.get_attribute('href')
+        if ref[: len(link_template)] + ref[link_template:] == link_template:
+            driver.get(ref)
+            elements = [item.find_element(By.TAG_NAME, 'a') for item in driver.find_elements(By.XPATH, "//*[contains(@class, 'tocGalleys')]")]
+            for index, element in enumerate(elements):
+                driver.get(element)
+                download_link = driver.find_element(By.XPATH, "//*[contains(@class, 'action')]").get_attribute('href')
+                print('Download link: ', download_link)
+                
+                try:
+                    # download_link = driver.find_element(By.CLASS_NAME, 'download').get_attribute('href')
+                    print("Enlace de descarga:", download_link)
+                    
+                    response = requests.get(download_link)
+                    pdf_path = os.path.join(current_path, f"{index}.pdf")
+                    print('Pdf path: ', pdf_path)
+                    
+                    with open(pdf_path, "wb") as f:
+                        f.write(response.content)
 
-print('Time Slaices Count: ', len(time_slices_refs))
+                    print(f"PDF descargado con éxito en {pdf_path}.")
+
+                except Exception as e:
+                    print(f"Error al descargar el PDF para '{title}': {e}")
+                    print('\\'*50)
+                    pending_doc.append(download_link)
+                    
+                    
+print('Time Slices Count: ', len(time_slices_refs))
 print('Example: ', time_slices_refs[0])
+
 
 # periods_link = [(item.find_element(By.TAG_NAME, 'h2').find_element(By.TAG_NAME, 'a').text, item.find_element(By.TAG_NAME, 'h2').find_element(By.TAG_NAME, 'a').get_attribute('href')) for item in time_periods]
 
