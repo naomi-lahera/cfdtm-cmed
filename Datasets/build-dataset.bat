1ed1a3e85e212188195edb279e9fbed91904853f
nao_lahera <lahera0103@gmail.com>
2025-02-18 19:32:05 -0500

    Crapeta Datasets organizada

--- Datasets/build-dataset.bat
+++ Datasets/build-dataset.bat
@@ -1,8 +1,6 @@
 @echo off
-REM Cambia "mi_script.py" al nombre de tu script de Python
-REM %1 y %2 son los parámetros para las rutas de las carpetas
 python download-dataset-selenium.py 
 python extract-text.py 
 python pre-build-dataset.py 
-python extract-features.py 
+python build-dataset.py 
 pause
--- Datasets/build-dataset.py
+++ Datasets/build-dataset.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 import scipy.sparse
 from topmost.preprocessing import Preprocessing
 import topmost.preprocessing.preprocessing as prepro
+import importlib
 from topmost.utils.logger import Logger
 import fasttext
 import fasttext.util
@@ -19,24 +20,11 @@ fasttext.util.download_model('es', if_exists='ignore')  # Español
 def make_word_embeddings_es(vocab):
     # Cargar el modelo binario preentrenado
     fasttext_model = fasttext.load_model("cc.es.300.bin")
-
-    # glove_vectors = gensim.downloader.load('glove-wiki-gigaword-200')
     word_embeddings = np.zeros((len(vocab), fasttext_model.get_dimension()))
 
     num_found = 0
 
-    # try:
-        # key_word_list = glove_vectors.index_to_key
-    # except:
-        # key_word_list = glove_vectors.index2word
-
-    # for i, word in enumerate(tqdm(vocab, desc="loading word embeddings")):
-        # if word in key_word_list:
-            # word_embeddings[i] = fasttext_model.get_word_vector(word)
-            # # word_embeddings[i] = glove_vectors[word]
-            # num_found += 1
-
-    for i, word in enumerate(tqdm(vocab, desc="loading word embeddings")):
+    for i, word in enumerate(tqdm(vocab, desc="loading SPANISH 🍀 word embeddings")):
         word_embeddings[i] = fasttext_model.get_word_vector(word)
         num_found += 1
 
@@ -44,9 +32,7 @@ def make_word_embeddings_es(vocab):
 
     return scipy.sparse.csr_matrix(word_embeddings)
 
-prepro.preprocessing.make_word_embeddings = make_word_embeddings_es
-
-    
+prepro.make_word_embeddings = make_word_embeddings_es
 
 if __name__ == '__main__':
     args = get_args_json(files.build_dataset)
@@ -57,8 +43,9 @@ if __name__ == '__main__':
     os.makedirs(jsonlist_path, exist_ok=True)
     os.makedirs(output_path, exist_ok=True)
 
+
     preprocessing = Preprocessing(stopwords=stopwords_es)
 
     rst = preprocessing.preprocess_jsonlist(dataset_dir=jsonlist_path, label_name="label")
 
-    preprocessing.save(output_path, **rst)    
+    preprocessing.save(output_path, **rst)  
