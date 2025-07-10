8428d17cd36f85cb66e7927aefbad654039ca6c4
Naomi Lahera <naomi@ingeniuscuba.com>
2025-07-10 04:20:21 -0400

    Extraccion del texto de los pdfs. Construccion de una lista de jsons que contiene a todos los articulos con su anno y label

--- src/dataset/BUILD-DATASET.bat
+++ src/dataset/BUILD-DATASET.bat
@@ -1,6 +1,7 @@
 @echo off
 REM Paths
 set DATASET_PAPERS_PATH=..\..\data\papers-pdf
+set DATASET_XML_PATH=..\..\data\papers-xml
 set DATASET_JSONLIST_PATH=..\..\data\texts-jsonlists
 set DATASET_PATH=..\..\data\CMed-dataset
 
@@ -10,7 +11,7 @@ REM Step #1: Download papers
 REM -
 
 REM Step #2: Extract text from papers (pdfs)
-python extract_text.py --input %DATASET_PAPERS_PATH% --output %DATASET_JSONLIST_PATH% 
+python extract_text.py --input %DATASET_PAPERS_PATH% --output_xml %DATASET_XML_PATH% --output_jsonlists %DATASET_JSONLIST_PATH%
 
 REM echo === Done ✅ ===
 REM pause
